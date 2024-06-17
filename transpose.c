24cbd2dfadba2d0172e89991509d6b872133b4d9
nilucf19 <167556172+nilucf19@users.noreply.github.com>
2024-06-17 22:29:42 +0530

    Create transpose.c

---
+++ transpose.c
@@ -0,0 +1,26 @@
+// Matrix Addition
+#include <stdio.h>
+
+int main() {
+  printf("Enter rows and cols of mat: "); 
+  int row, col;
+  scanf("%d%d", &row, &col);
+  for(int i = 0; i < row; ++i) {
+    for(int j = 0; j < col; ++j) {
+      scanf("%d", &mat[i][j]);
+    }
+  }
+
+ for(int i = 0; i < ro; ++i) {
+    for(int j = 0; j < col; ++j) {
+      trans[j][i] = mat[i][j]
+    }
+ }
+  
+  for(int i = 0; i < col; ++i) {
+    for(int j = 0; j < row; ++j) {
+      printf("%d ", trans[i][j]);
+    }
+    printf("\n");
+  }
+}
