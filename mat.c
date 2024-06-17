d121aa31a0f2c8e5d2363b5fb0e39303882d93ef
nilucf19 <167556172+nilucf19@users.noreply.github.com>
2024-06-17 21:53:49 +0530

    Update mat.c

--- mat.c
+++ mat.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
 int main() {
-  printf("Enter rows and cols of matrix : ");
-  int row, col;
+  printf("Enter rows and cols of mat1: ");
+  int row1, col1;
   scanf("%d%d", &row, &col);
-  int mat[row][col];
+  int mat1[row1][col1];
 
-  printf("Enter the elements : \n");
-  for(int i = 0; i < row; ++i) {
-    for(int j = 0; j < col; ++j) {
+  printf("Enter the elements of mat1: \n");
+  for(int i = 0; i < row1; ++i) {
+    for(int j = 0; j < col1; ++j) {
+      scanf("%d", &mat[i][j]);
+    }
+  }
+
+  int row2, col2;
+  scanf("%d%d", &row2, &col2);
+  int mat2[row2][col2];
+  printf("Enter the elements of mat2: \n");
+  for(int i = 0; i < row2; ++i) {
+    for(int j = 0; j < col2; ++j) {
       scanf("%d", &mat[i][j]);
     }
   }
